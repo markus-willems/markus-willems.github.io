4151085e5c07059dd80aefbc6742d55129717a08
Markus Willems <mail@markus-willems.de>
2017-07-16 16:34:30 +0200

    New build

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
-const path = require('path');
+const path = require('path')
 
 // plugins
-const webpack = require('webpack');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const DIST_PATH = path.resolve(__dirname, 'dist');
-const SRC_PATH = path.resolve(__dirname, 'src');
+const DIST_PATH = path.resolve(__dirname, 'dist')
+const SRC_PATH = path.resolve(__dirname, 'src')
 
-let extractCSS = new ExtractTextPlugin({ filename: 'css/style.css', allChunks: true });
+let extractCSS = new ExtractTextPlugin({
+  filename: 'css/style.[contenthash].css',
+  allChunks: true,
+})
 
 const config = {
   entry: SRC_PATH + '/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash].js',
     path: DIST_PATH,
     publicPath: '/dist/',
   },
@@ -21,28 +24,26 @@ const config = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: [
-          {loader: 'babel-loader'}
-        ]
+        use: [{ loader: 'babel-loader' }],
       },
       {
         test: /\.css$/,
         use: extractCSS.extract({
           fallback: 'style-loader',
-          use: 'css-loader'
-        })
-      }
-    ]
+          use: 'css-loader',
+        }),
+      },
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
     new webpack.optimize.UglifyJsPlugin(),
     extractCSS,
-  ]
+  ],
 }
 
-module.exports = config;
+module.exports = config
