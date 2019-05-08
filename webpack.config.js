6467da6374ec28eeb9a58acf09702d2643e911bc
Markus Willems <mail@markus-willems.de>
2019-05-08 22:35:07 +0200

    Add new website version

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,13 @@
 const path = require('path');
 
-// plugins
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const DIST_PATH = path.resolve(__dirname, 'dist');
-const SRC_PATH = path.resolve(__dirname, 'src');
-
-let extractCSS = new ExtractTextPlugin({
-  filename: 'css/style.css',
-  allChunks: true,
-});
-
-const config = {
-  entry: SRC_PATH + '/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: DIST_PATH,
-    publicPath: '/dist/',
-  },
-  devServer: {
-    host: '0.0.0.0',
-    allowedHosts: ['markus-gos'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [{ loader: 'babel-loader' }],
-      },
-      {
-        test: /\.css$/,
-        use: extractCSS.extract({
-          fallback: 'style-loader',
-          use: 'css-loader',
-        }),
-      },
-    ],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-    new webpack.optimize.UglifyJsPlugin(),
-    extractCSS,
-  ],
+const config = () => {
+	return {
+		entry: './src/index.js',
+		output: {
+			filename: 'bundle.js',
+			path: path.resolve(__dirname, 'dist'),
+		},
+	};
 };
 
 module.exports = config;
